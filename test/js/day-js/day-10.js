61d8894f4c2d3d1189445b21ddbb76a003f66541
t0b1 <39978666+Kuber12@users.noreply.github.com>
2021-08-14 11:32:08 +0545

    Add files via upload

--- test/js/day-js/day-10.js
+++ test/js/day-js/day-10.js
@@ -1,2 +1,14 @@
-var joke = document.getElementsByClassName('joke');
-var getButton = document.getElementsByClassName('get-button')
+const joke = document.querySelector('.joke');
+const getButton = document.querySelector('.get-button');
+getJoke();
+getButton.addEventListener('click',getJoke);
+
+function getJoke(){
+fetch('https://icanhazdadjoke.com',{
+    headers:{
+        'Accept':'application/json'  
+    }
+})
+  .then(response => response.json())
+  .then(data => joke.innerHTML = data.joke);
+}
