cd617a58ce57872a0982c4858f6983897c91d26c
t0b1 <39978666+Kuber12@users.noreply.github.com>
2022-05-25 09:27:24 +0545

    Delete test directory

--- test/js/day-js/day-7.js
+++
@@ -1,7 +0,0 @@
-var rightOver = document.querySelector('.right');
-var leftOver = document.querySelector('.left');
-var container = document.querySelector('section')
-rightOver.addEventListener('mouseenter',() =>{container.classList.add('hover-right')});
-rightOver.addEventListener('mouseleave',() =>{container.classList.remove('hover-right')});
-leftOver.addEventListener('mouseenter',() =>{container.classList.add('hover-left')});
-leftOver.addEventListener('mouseleave',() =>{container.classList.remove('hover-left')});
